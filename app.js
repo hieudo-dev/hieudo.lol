0080352fbe67432abf36a05561953e610e4f3e5c
Hieu Do <hieudo1@beeketing.net>
2023-09-29 00:20:01 +0700

    add websockets server

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ const app = express();
 const server = http.createServer()
 
 app.get('/', function(req, res) {
-	res.write("Hello world")
-	res.end()
+	res.sendFile('index.html', { root: __dirname })
 })
 server.on('request', app)
 server.listen(3000)
 
 console.log("Server started on port 3000")
+
+//--------------------------
+const ws = require('ws')
+
+const wss = new ws.Server({server}) 
+wss.on('connection', (connection) => {
+	const numClients = wss.clients.size
+	wss.broadcast('Current visitors: ' + numClients)
+	if (ws.readyState === ws.OPEN) {
+		ws.send('Welcome to my server!')
+	}
+})
+
+wss.broadcast = (msg) => wss.clients.forEach(client => client.send(msg))
