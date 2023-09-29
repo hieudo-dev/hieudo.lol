12269458cb4e2422d05607e0859e7bbfc42140e4
Hieu Do <hieudo1@beeketing.net>
2023-09-29 12:37:22 +0700

    add database

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get('/', function(req, res) {
 server.on('request', app)
 server.listen(3000)
 
+process.on('SIGINT', () => {
+	wss.clients.forEach(client => client.close())
+	server.close(() => shutdownDB())
+})
+
 console.log("Server started on port 3000")
 
 //--------------------------
@@ -22,6 +27,37 @@ wss.on('connection', (connection) => {
 	if (ws.readyState === ws.OPEN) {
 		ws.send('Welcome to my server!')
 	}
+	
+	db.run(`
+		INSERT INTO visitors(count, time) VALUES (${numClients}, DATE('now'));
+	`)
+	connection.on('close', () => wss.broadcast('A client disconnected'))
 })
 
 wss.broadcast = (msg) => wss.clients.forEach(client => client.send(msg))
+
+
+//--------------------------
+const sqlite = require('sqlite3')
+const db = new sqlite.Database(':memory:')
+
+db.serialize(() => {
+	db.run(`
+		CREATE TABLE IF NOT EXISTS visitors (
+			count INTEGER,
+			time TEXT
+		)
+	`)
+})
+
+function getCounts() {
+	db.each("SELECT * FROM visitors", (err, row) => {
+		console.log('Row data: ', row)
+	})
+}
+
+function shutdownDB() {
+	getCounts()
+	console.log("Shutting down db")
+	db.close()
+}
